e3638c6be7c14c3f050a2d6bec022cbaa99a5b91
Marcel Witte <marcel@aliquis.de>
2022-07-17 22:36:53 +0200

    generate a findAllBy method for fields annotated with @Searchable

--- modules/generator/src/main/java/cloud/quinimbus/magic/generator/RepositoryGenerator.java
+++ modules/generator/src/main/java/cloud/quinimbus/magic/generator/RepositoryGenerator.java
@@ -4,8 +4,12 @@ import cloud.quinimbus.magic.elements.MagicClassElement;
 import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.CodeBlock;
+import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
+import java.util.Locale;
+import java.util.stream.Stream;
 import javax.lang.model.element.Modifier;
 
 public class RepositoryGenerator extends RecordEntityBasedGenerator {
@@ -26,6 +30,21 @@ public class RepositoryGenerator extends RecordEntityBasedGenerator {
                                 ClassName.get("cloud.quinimbus.persistence.repositories", "CRUDRepository"),
                                 ClassName.get(packageName, name),
                                 ClassName.get(idType)));
+        recordElement.findFieldsAnnotatedWith("cloud.quinimbus.persistence.api.annotation.Searchable")
+                .forEach(ve -> {
+                    repositoryTypeBuilder.addMethod(
+                            MethodSpec.methodBuilder("findAllBy%s".formatted(capitalize(ve.getSimpleName().toString())))
+                                    .addModifiers(Modifier.ABSTRACT, Modifier.PUBLIC)
+                                    .addParameter(TypeName.get(ve.asType()), "value")
+                                    .returns(ParameterizedTypeName.get(
+                                            ClassName.get(Stream.class),
+                                            ClassName.get(packageName, name)))
+                                    .build());
+                });
         return repositoryTypeBuilder.build();
+    }
+    
+    private static String capitalize(String str) {
+        return str.substring(0, 1).toUpperCase(Locale.US).concat(str.substring(1));
     }
 }
